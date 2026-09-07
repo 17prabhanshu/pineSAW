177b33947649792237bd7db047fdb0c6dc64fde6
Prabhanshu Shekhar <prabhanshushekhar@users.noreply.github.com>
2026-09-07 19:12:37 +0530

    Add intense white glow to the background watermark logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({
           
           {/* Faded Watermark Background */}
           <div className="fixed inset-0 pointer-events-none z-0 flex items-center justify-center opacity-[0.15]">
-            <img src="/cp-logo.png" alt="Watermark" className="w-[800px] h-[800px] object-contain" />
+            <img src="/cp-logo.png" alt="Watermark" className="w-[800px] h-[800px] object-contain drop-shadow-[0_0_30px_rgba(255,255,255,0.8)]" />
           </div>
 
           {/* Main Application Area */}
